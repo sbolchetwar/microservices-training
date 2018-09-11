278c1674c41989c3619badd47f0cca9b9f52ded7
sandeep <sbolchetwar@live.com>
2018-09-11 17:32:37 +0530

    Active MQ demo example

---
+++ Spring-ActiveMq/src/main/java/demo/Consumer.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+    @JmsListener(destination = "standalone.queue")
+    public void consume(String message) {
+        System.out.println("Received Message: " + message);
+    }
+}
---
+++ Spring-ActiveMq/src/main/java/demo/SpringActiveMqApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActiveMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActiveMqApplication.class, args);
+	}
+}
