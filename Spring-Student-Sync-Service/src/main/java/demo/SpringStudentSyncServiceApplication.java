867e025cdcd916d1ab35d2be6347172cfbd6b7ec
sandeep <sbolchetwar@live.com>
2018-09-12 12:39:09 +0530

    Interprocess sync call service demo example

---
+++ Spring-Student-Sync-Service/src/main/java/demo/SpringStudentSyncServiceApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudentSyncServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudentSyncServiceApplication.class, args);
+	}
+}
